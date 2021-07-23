d4f542fcce1b57f8bacdaee4cb8346b7b3298838
Robert Tsyura <87837424+Robsth@users.noreply.github.com>
2021-07-23 03:33:19 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+"""
+II Задача о парковке ⭐⭐
+
+Дана последовательность пар вида {start, end}
+
+start - время заезда автотранспорта на парковку
+end - время выезда автотранспорта с парковки
+Нужно найти максимальное количество автотранспорта, находящихся одновременно на парковке
+
+Что хочется увидеть:
+
+Алгоритм с оптимальной реализацией по памяти
+"""
+
+from datetime import datetime
+
+
+parking_time_tracking = [
+    ('09:00', '13:00'),
+    ('05:15', '08:59'),
+    ('01:10', '04:18'),
+    ('04:30', '15:00'),
+    ('00:30', '02:18'),
+    ('01:16', '01:48'),
+    ('01:37', '09:48'),
+    ('18:32', '22:22'),
+    ('19:25', '23:18'),
+    ('22:01', '22:18'),
+]
+
+
+def time_parsing(time_periods: str) -> datetime:
+    return datetime.strptime(time_periods, '%H:%M')
+
+
+def mark_and_sort(time_periods: list) -> list:
+    time_sets = []
+
+    for single_car_period in time_periods:
+        start_time, end_time = single_car_period
+        time_sets.append((time_parsing(start_time), 'start'))
+        time_sets.append((time_parsing(end_time), 'end'))
+
+    return sorted(time_sets)
+
+
+def get_max_car_count(time_sets: list) -> int:
+    marked_times = mark_and_sort(time_sets)
+
+    count = 0
+    max_count = 0
+
+    for time in marked_times:
+        if 'start' in time:
+            count += 1
+        else:
+            count -= 1
+        max_count = max(count, max_count)
+    return max_count
+
+
+if __name__ == '__main__':
+    print(
+        f'Максимальное количество автотранспорта, находящегося одновременно на парковке: '
+        f'{get_max_car_count(parking_time_tracking)}'
+    )
+
